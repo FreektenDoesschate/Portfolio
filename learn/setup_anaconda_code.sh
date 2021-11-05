aff6f547755a27ff4fcb862e7219d3887ef071fa
FreektenDoesschate <43320512+FreektenDoesschate@users.noreply.github.com>
2021-11-05 11:55:47 +0100

    Update setup_anaconda_code.sh

--- learn/setup_anaconda_code.sh
+++ learn/setup_anaconda_code.sh
@@ -1,4 +1,4 @@
-#run code in terminal with anaconda already installed
+#run code in terminal with anaconda already installed. Change 'myenv' instances with how you want to name your environment. See setup_anaconda_env.md to configure jupyterlab-lsp
 
 conda config --append channels conda-forge
 conda create -n myenv -y python=3.9 jupyterlab jupyterlab-lsp jupyterlab_code_formatter black isort numpy pandas matplotlib plotly scikit-learn xgboost dalex
