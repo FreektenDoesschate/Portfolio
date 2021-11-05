f75f438e0e3c27522ea46e8b86def65551964693
FreektenDoesschate <43320512+FreektenDoesschate@users.noreply.github.com>
2021-11-05 11:54:49 +0100

    Update setup_anaconda_code.sh

--- learn/setup_anaconda_code.sh
+++ learn/setup_anaconda_code.sh
@@ -1,5 +1,7 @@
+#run code in terminal with anaconda already installed
 
 conda config --append channels conda-forge
 conda create -n myenv -y python=3.9 jupyterlab jupyterlab-lsp jupyterlab_code_formatter black isort numpy pandas matplotlib plotly scikit-learn xgboost dalex
 conda activate myenv
 pip install 'python-lsp-server[all]' lckr-jupyterlab-variableinspector aquirdturtle_collapsible_headings
+
